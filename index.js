488e93b14c6c6d079851c1fac9a38e4acce30515
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-11-25 21:45:31 -0500

    [+] index.js: Define GET /, GET /health

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require( 'express' );   // Importacion
 const app = express();                  // Invocando core Express
 const PORT = 3000;                      // Definiendo el puerto de escucha
 
+// Definir las rutas
+app.get( '/', ( req, res ) => {
+    res.send( '<h1>Home</h1>' );
+} );
+
+app.get( '/health', ( req, res ) => {
+    res.send( '<h1>Health</h1>' );
+} );
+
 // Lanzando el servidor web usando Express
 app.listen( PORT, () => {
     console.log( `Server running on http://localhost:${ PORT } :)` );
