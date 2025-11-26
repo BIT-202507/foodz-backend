8a27cf52b30c4ae6578c5f8a10b8abaac3a22a27
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-11-25 21:27:34 -0500

    [+] package.json: Define script 'dev' [+] index.js: Carita Feliz

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = 3000;                      // Definiendo el puerto de escucha
 
 // Lanzando el servidor web usando Express
 app.listen( PORT, () => {
-    console.log( `Server running on http://localhost:${ PORT }` );
+    console.log( `Server running on http://localhost:${ PORT } :)` );
 } );
