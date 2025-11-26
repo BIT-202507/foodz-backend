0c2db545db2234def5b8864c620381e4d65e8a8c
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-11-25 21:08:51 -0500

    [+] npm i express [+] .gitignore [+] index.js: Lanza servidor usando Express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log( 'Project FoodZ' );
+const express = require( 'express' );   // Importacion
+
+const app = express();                  // Invocando core Express
+const PORT = 3000;                      // Definiendo el puerto de escucha
+
+// Lanzando el servidor web usando Express
+app.listen( PORT, () => {
+    console.log( `Server running on http://localhost:${ PORT }` );
+} );
