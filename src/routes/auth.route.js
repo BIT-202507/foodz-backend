95a2ab82c0e42446585a8f7ae050362cfb52a1e7
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-12-13 19:06:25 -0500

    [+] *.routes.js: Se implementan los Middlewares en todas las rutas que deben ser privadas [+] without-role.js: POST /api/v1/users para eliminar la propiedad 'role' y garanizar que no se puede personalizar el rol de usuario (por defecto siempre queda 'registered')

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,19 +1,21 @@
 import { Router } from "express";
+
 import { loginUser, reNewtoken } from "../controllers/auth.controller.js";
 import { createUser } from "../controllers/user.controller.js";
 import { authenticationUser } from "../middlewares/authetication.middleware.js";
 import { authorizationUser } from "../middlewares/authorization.middleware.js";
+import { withoutRole } from "../middlewares/without-role.middleware.js";
 
 const router = Router();
 
 // Definir las rutas para la creacion, autenticacion, renovacion del token
 // http://localhost:3000/api/v1/auth
 router.post('/login', loginUser);
-router.post('/register', createUser);       // Registrar usuarios publica
+router.post('/register', withoutRole, createUser);       // Registrar usuarios publica
 router.get(
-    '/renew-token',     // Path
-    [authenticationUser, authorizationUser],                 // Middlewares
-    reNewtoken          // Controller
+    '/renew-token',                             // Path
+    [authenticationUser, authorizationUser],    // Middlewares
+    reNewtoken                                  // Controller
 );
 
 
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,16 +2,38 @@
 import { Router } from 'express';       // Desestructurando Router desde express
 
 import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from '../controllers/user.controller.js';
+import { authenticationUser } from '../middlewares/authetication.middleware.js';
+import { authorizationUser } from '../middlewares/authorization.middleware.js';
 
 const router = Router();
 
 // Definicion de las rutas (EndPoints)
 // http://localhost:3000/api/v1/users
-router.post('/', createUser);     // Registrar usuarios privado (Administador/Loguarse)
-router.get('/', getAllUsers);
-router.get('/:idUser', getUserById);  // Parametrizar la ruta: Crear un parametro en la ruta que funje como variable
-router.delete('/:idUser', deleteUserById);
-router.patch('/:idUser', updateUserById);
+router.post(
+    '/',                                        // Path
+    [authenticationUser, authorizationUser],    // Middlewares
+    createUser                                  // Controller
+);     // Registrar usuarios privado (Administador/Loguarse)
+router.get(
+    '/',                                        // Path
+    [authenticationUser, authorizationUser],    // Middlewares
+    getAllUsers                                 // Controller
+);
+router.get(
+    '/:idUser',                                 // Path
+    [authenticationUser, authorizationUser],    // Middlewares
+    getUserById                                 // Controller
+);  // Parametrizar la ruta: Crear un parametro en la ruta que funje como variable
+router.delete(
+    '/:idUser',                                 // Path
+    [authenticationUser, authorizationUser],    // Middlewares
+    deleteUserById                              // Controller
+);
+router.patch(
+    '/:idUser',                                 // Path
+    [authenticationUser, authorizationUser],    // Middlewares
+    updateUserById                              // Controller
+);
 
 // Exportando el router usando CommonJS
 export default router;
